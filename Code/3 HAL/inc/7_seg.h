df0a3c3005f15085c00cb77438ee88a136a69d8e
Shaheen <mohamedshaheen1514@gmail.com>
2022-10-13 18:58:08 +0200

    check  7 seg .h

--- Code/3 HAL/inc/7_seg.h
+++ Code/3 HAL/inc/7_seg.h
@@ -1,13 +1,36 @@
+/********************************************************************/
+/********************************************************************/
+/**************       Author : Moahmed A Shaheen     *****************/
+/**************       Layer : HAL                  *****************/
+/**************       SWC:    _7SEG_H_                  *****************/
+/**************       Version : 1.0                 *****************/
+/**************       Date : 13/10/2022              *****************/
 
-////////////////////////////////////////////////////////////////////////////////
-///////////////////////    EDITOR:      TEAM A /////////////////////////////////
-///////////////////////    SW Compponent: APP /////////////////////////////////
-///////////////////////    SW : 7_Seqment_H_     /////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
+
+#ifndef  _7SEG_H_
+#define  _7SEG_H_
 
 
-#ifndef _7SEG_H_
-#define _7SEG_H_
+
+/*Functon Prototypes *******************************************/
+/*
+* Description : This function is used to initialize the 7 seqment
+* Parameters  : 
+*               
+* Return      : void
+*/
+void _7seq_voidInit();
+
+/*Functon Prototypes *******************************************/
+/*
+* Description : This function is used to initialize the 7 seqment
+* Parameters  : 
+*               
+* Return      : void
+*/
+uint8_t _7seq_voidInit(uint8_t 	Copy_u8Port ,  uint8_t Copy_u8Mode);
+
+uint8_t _7seq_voidWrite(uint8_t 	Copy_u8Port , uint8_t Copy_u8Pin , uint8_t Copy_u8Mode);
 
 
 #endif
