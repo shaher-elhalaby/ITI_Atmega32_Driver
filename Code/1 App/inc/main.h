debad091da57b79407117ea90ce4b4e33d48802e
shaher Elhalaby <shaher.elhalaby@gmail.com>
2022-10-16 13:05:21 +0200

    Updating HAL DIO

--- Code/1 App/inc/main.h
+++ Code/1 App/inc/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H_
 
 // Include Files
-
+#include "HDIO.h"
 // Function Prototypes
 
 #endif /* MAIN_H_ */
--- Code/1 App/src/main.c
+++ Code/1 App/src/main.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
 int main(void) {
+  HAL_DIO_initTypedef pa1;
+  pa1.channel = HAL_DIO_PIN_1;
+  pa1.mode = HAL_DIO_MODE_OUTPUT;
+  HAL_DIO_pinInit(&pa1);
   while (1) {
+    HAL_DIO_writeTypedef pa1;
+    pa1.channel = HAL_DIO_PIN_1;
+    pa1.value = 1;
+    HAL_DIO_pinWrite(&pa1);
+
+    pa1.value = 0;
+    HAL_DIO_pinWrite(&pa1);
   }
 }
